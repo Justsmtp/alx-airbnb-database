7b25d85560f52972fedd2a92cb8e00b344e2b2b4
Justsmtp <smtpwtf@gmail.com>
2025-07-02 00:14:13 +0100

    New files

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,18 @@
+-- Non-correlated subquery: Find all properties where the average rating is greater than 4.0
+SELECT property_id, name, address
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- Correlated subquery: Find users who have made more than 3 bookings
+SELECT user_id, name, email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
