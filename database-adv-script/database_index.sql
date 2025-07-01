941b3409b68fc72f416bf82083f496e36842b1a6
Justsmtp <smtpwtf@gmail.com>
2025-07-02 00:38:29 +0100

    New files

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,11 +1,17 @@
--- Create index on user_id in users table
-CREATE INDEX idx_users_user_id ON users(user_id);
+USE alx_book_store;
 
--- Create index on user_id in bookings table
+-- Measure before adding indexes
+EXPLAIN SELECT users.user_id, COUNT(bookings.booking_id)
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+GROUP BY users.user_id;
+
+-- Add indexes
+CREATE INDEX idx_users_user_id ON users(user_id);
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 
--- Create index on property_id in properties table
-CREATE INDEX idx_properties_property_id ON properties(property_id);
-
--- Create index on property_id in bookings table
-CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+-- Measure after adding indexes
+EXPLAIN SELECT users.user_id, COUNT(bookings.booking_id)
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+GROUP BY users.user_id;
