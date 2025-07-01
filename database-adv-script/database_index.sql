2faf85de1221ba8fbf9bf436385af5c91c70a69f
Justsmtp <smtpwtf@gmail.com>
2025-07-02 00:40:43 +0100

    New files

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,17 +1,20 @@
 USE alx_book_store;
 
--- Measure before adding indexes
-EXPLAIN SELECT users.user_id, COUNT(bookings.booking_id)
+-- Measure BEFORE adding indexes
+EXPLAIN ANALYZE
+SELECT users.user_id, COUNT(bookings.booking_id) AS total_bookings
 FROM users
 LEFT JOIN bookings ON users.user_id = bookings.user_id
 GROUP BY users.user_id;
 
--- Add indexes
+-- Create indexes
 CREATE INDEX idx_users_user_id ON users(user_id);
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_properties_property_id ON properties(property_id);
 
--- Measure after adding indexes
-EXPLAIN SELECT users.user_id, COUNT(bookings.booking_id)
+-- Measure AFTER adding indexes
+EXPLAIN ANALYZE
+SELECT users.user_id, COUNT(bookings.booking_id) AS total_bookings
 FROM users
 LEFT JOIN bookings ON users.user_id = bookings.user_id
 GROUP BY users.user_id;
