347405e1fee2d3a124e5244f16f0b9907bef5764
Justsmtp <smtpwtf@gmail.com>
2025-07-02 00:16:31 +0100

    New files

---
+++ database-adv-script/aggregations_and_window.sql
@@ -0,0 +1,21 @@
+-- Query 1: Total number of bookings made by each user
+SELECT
+    users.user_id,
+    users.name,
+    COUNT(bookings.booking_id) AS total_bookings
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+GROUP BY users.user_id, users.name
+ORDER BY total_bookings DESC;
+
+
+-- Query 2: Rank properties based on total number of bookings
+SELECT
+    properties.property_id,
+    properties.name AS property_name,
+    COUNT(bookings.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(bookings.booking_id) DESC) AS booking_rank
+FROM properties
+LEFT JOIN bookings ON properties.property_id = bookings.property_id
+GROUP BY properties.property_id, properties.name
+ORDER BY booking_rank;
